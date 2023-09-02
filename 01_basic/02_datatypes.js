029e043602c022f4551f2e6117ff593e8eab43f2
Nirav Patel <109053890+nirav371@users.noreply.github.com>
2023-09-02 07:32:11 +0000

    data types

--- 01_basic/02_datatypes.js
+++ 01_basic/02_datatypes.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("nirav")
 
 
-let name = "hitesh"
+let name = "nirav"
 let age = 18
 let isLoggedIn = false
 let state;
