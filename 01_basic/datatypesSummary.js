81ffbd31613388d5f78cec47d17b77afc44ad4b8
nirav patel <nirav1392002@gmail.com>
2023-09-28 16:03:48 +0530

    add notes

--- 01_basic/datatypesSummary.js
+++ 01_basic/datatypesSummary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+console.log(id === anotherId);//false
 
 // const bigNumber = 3456543576654356754n
 
