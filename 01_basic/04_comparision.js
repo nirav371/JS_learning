321301f594b03ef06e1e4907765e233096e3f125
nirav patel <nirav1392002@gmail.com>
2023-09-28 12:53:00 +0530

    add notes

--- 01_basic/04_comparision.js
+++ 01_basic/04_comparision.js
@@ -8,9 +8,10 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0); // false
+console.log(null == 0); // false
+console.log(null >= 0); // true
+// == ane >= work diffrently that's in one case output is true in another false. null ni value 0 j che
 
 console.log(undefined == 0);
 console.log(undefined > 0);
