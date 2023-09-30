8363099d2682d9e2fb9c85be7392a06804aff8da
nirav patel <nirav1392002@gmail.com>
2023-10-01 00:53:58 +0530

    notes

--- 01_basic/05_strings.js
+++ 01_basic/05_strings.js
@@ -5,7 +5,7 @@ const repoCount = 50
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('nirav-hc-com')
+const gameName = new String('nirav-hc-com') // for protype view you can type this in console then call this in console
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
