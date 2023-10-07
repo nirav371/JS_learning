fcf467cfc1c6c025890ba724997372f7010f1b8c
nirav patel <nirav1392002@gmail.com>
2023-10-07 22:12:25 +0530

    arrow

--- 03_basic/03_arrow.js
+++ 03_basic/03_arrow.js
@@ -1,35 +1,38 @@
 const user = {
-    username: "hitesh",
-    price: 999,
+    username: "nirav",
+    price: 99,
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        console.log(this); //this keyword current context ni vat kare. what is current. current value ne refer kare
+        // global object  == window object --> event capture
+        // this keyword we can only use in object. we can't use in only function
     }
 
 }
 
 // user.welcomeMessage()
 // user.username = "sam"
-// user.welcomeMessage()
+// user.welcomeMessage() // this keyword refer current context that's why output is sam, welcome to the website
 
 // console.log(this);
 
 // function chai(){
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this){...xyz}
+//     console.log(this.username); //output is undefined
 // }
 
 // chai()
 
 // const chai = function () {
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); //output is undefined
 // }
 
 const chai =  () => {
     let username = "hitesh"
-    console.log(this);
+    console.log(this); // output is {}
 }
 
 
@@ -43,10 +46,10 @@ const chai =  () => {
 
 // const addTwo = (num1, num2) => ( num1 + num2 )
 
-const addTwo = (num1, num2) => ({username: "hitesh"})
+const addTwo = (num1, num2) => ({username: "hitesh"}) // when we want to return without retun keyword then we use paranthisis() when we use curly brasis then we must use return keyword without return keyword we can't return.
 
 
-console.log(addTwo(3, 4))
+// console.log(addTwo(3, 4))
 
 
 // const myArray = [2, 5, 3, 7, 8]
