47953dfa647d203971e54c55c2e5bdf5c66c747f
nirav patel <nirav1392002@gmail.com>
2023-10-07 22:29:09 +0530

    IIFE

--- 03_basic/04_iife.js
+++ 03_basic/04_iife.js
@@ -1,11 +1,15 @@
 // Immediately Invoked Function Expressions (IIFE)
 
-
+// global scope ke polution se problem hoti he kai bar to global scope ke koi bhi variable he declaration he uske polution ko hatane ke liye hum iife ka use karte he
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})(); // semicolon is must in iife. without semicolon iife when context end it's can't decide.
+// chai() // output: chai is not defined --> it's saves us frome global scope polution
 
 ( (name) => {
+    // named IIFE
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+} )('Nirav')
+
+console.log('nirav')
